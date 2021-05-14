06b664232ae3541c9b889ac2044d9a3e64e937cf
Berezentsev Evgeny <jonn09@icloud.com>
2021-05-14 19:22:11 +0300

    Initial Commit

---
+++ TableViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by Евгений Березенцев on 14.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
